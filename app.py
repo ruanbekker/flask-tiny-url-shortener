aa955ff28a81345a80cbf2d1db76ebae9ba0afc5
Ruan Bekker <ruan.ru.bekker@gmail.com>
2018-09-19 23:22:26 +0200

    add function to write to simpledb

--- app.py
+++ app.py
@@ -15,6 +15,19 @@ def generate_tiny():
     tiny_key = ''.join(choice(ascii_letters) + str(randint(0,9)) for x in range(randint(8, 8)))
     return tiny_key
 
+def put_url(destination_url):
+    tiny_id = generate_tiny()
+    tiny_url = '{}/{}'.format(tiny_baseurl, tiny_id)
+    response = sdb.put_attributes(
+        DomainName=sdb_domain, 
+        ItemName=tiny_id, 
+        Attributes=[{
+            'Name': tiny_url, 
+            'Value': destination_url
+        }]
+    )
+    return tiny_url
+
 @app.route('/')
 def main():
     return 'ok'
